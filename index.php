7d9367972972b3c9db61e6b6c4a041992be92488
DenCode-Creator <dedenkusmana709@gmail.com>
2022-06-06 06:58:52 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
   <h1>
       Hello World!
   </h1>  
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, enim! Aliquam libero eveniet, pariatur quibusdam accusantium sint sequi enim reprehenderit maxime dolores totam alias sunt voluptas, accusamus, fugiat laboriosam quisquam!</p>
 </body>
 </html>
